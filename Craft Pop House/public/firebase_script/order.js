520a57e42c5a1d89c20f3f81800213dcc4f18df0
wellson99 <55526214+wellson99@users.noreply.github.com>
2020-05-06 19:10:58 +0800

    minor fix on order.js

---
+++ Craft Pop House/public/firebase_script/order.js
@@ -0,0 +1,28 @@
+//get user id
+var userID = sessionStorage.getItem("uid");
+//console.log(userID);
+
+
+var content = new Vue({
+    el: '#orderDetail_content',
+    data: {
+      products : []
+    },
+    methods: {
+		//do the onclick function here
+    },
+    mounted() {
+        const ref = firebase.firestore().collection("users").doc(userID).collection("orders");
+
+        ref.onSnapshot(snapshot =>{
+            //console.log(snapshot);
+        
+            let porductArr = [];
+            snapshot.forEach(doc => {
+                porductArr.push({...doc.data(), id: doc.id})
+            });
+            //console.log(snapshot);
+            this.products = porductArr;
+        });
+    },
+});

1d709ecb9f67a6250d9802c7ae2690a82b4554c6
wellson99 <55526214+wellson99@users.noreply.github.com>
2020-05-21 16:09:50 +0800

    Minor bug fixes
    Fix the bug where the seller product would not display in seller profile page
    Display seller profile pic in seller profile page & product detail page

--- Craft Pop House/public/scripts/filterCat.js
+++ Craft Pop House/public/scripts/filterCat.js
@@ -1,223 +1,224 @@
-function showAll(){
+function seller_showAll(){
     document.getElementById("catInfo").innerHTML = "Showing All Products";
 
-    var x = document.getElementById("all");
+    var x = document.getElementById("seller_all");
     x.style.display = "block";
 
-    var x = document.getElementById("clothOnly");
+    var x = document.getElementById("seller_clothOnly");
     x.style.display = "none";
 
-    var x = document.getElementById("jewOnly");
+    var x = document.getElementById("seller_jewOnly");
     x.style.display = "none";
 
-    var x = document.getElementById("craftOnly");
+    var x = document.getElementById("seller_craftOnly");
     x.style.display = "none";
 
-    var x = document.getElementById("bedOnly");
+    var x = document.getElementById("seller_bedOnly");
     x.style.display = "none";
 
-    var x = document.getElementById("toyOnly");
+    var x = document.getElementById("seller_toyOnly");
     x.style.display = "none";
 
-    var x = document.getElementById("artOnly");
+    var x = document.getElementById("seller_artOnly");
     x.style.display = "none";
 
-    var x = document.getElementById("weddingOnly");
+    var x = document.getElementById("seller_weddingOnly");
     x.style.display = "none";
 };
 
-function filterCloth(){
+function seller_filterCloth(){
     document.getElementById("catInfo").innerHTML = "Showing Products With Clothing & Accessories Category";
 
-    var x = document.getElementById("all");
+    var x = document.getElementById("seller_all");
     x.style.display = "none";
 
-    var x = document.getElementById("clothOnly");
+    var x = document.getElementById("seller_clothOnly");
     x.style.display = "block";
 
-    var x = document.getElementById("jewOnly");
+    var x = document.getElementById("seller_jewOnly");
     x.style.display = "none";
 
-    var x = document.getElementById("craftOnly");
+    var x = document.getElementById("seller_craftOnly");
     x.style.display = "none";
 
-    var x = document.getElementById("bedOnly");
+    var x = document.getElementById("seller_bedOnly");
     x.style.display = "none";
 
-    var x = document.getElementById("toyOnly");
+    var x = document.getElementById("seller_toyOnly");
     x.style.display = "none";
 
-    var x = document.getElementById("artOnly");
+    var x = document.getElementById("seller_artOnly");
     x.style.display = "none";
 
-    var x = document.getElementById("weddingOnly");
+    var x = document.getElementById("seller_weddingOnly");
     x.style.display = "none";
 };
 
-function filterJew(){
+function seller_filterJew(){
     document.getElementById("catInfo").innerHTML = "Showing Products With Jewelry Category";
 
-    var x = document.getElementById("all");
+    var x = document.getElementById("seller_all");
     x.style.display = "none";
 
-    var x = document.getElementById("clothOnly");
+    var x = document.getElementById("seller_clothOnly");
     x.style.display = "none";
 
-    var x = document.getElementById("jewOnly");
+    var x = document.getElementById("seller_jewOnly");
     x.style.display = "block";
 
-    var x = document.getElementById("craftOnly");
+    var x = document.getElementById("seller_craftOnly");
     x.style.display = "none";
 
-    var x = document.getElementById("bedOnly");
+    var x = document.getElementById("seller_bedOnly");
     x.style.display = "none";
 
-    var x = document.getElementById("toyOnly");
+    var x = document.getElementById("seller_toyOnly");
     x.style.display = "none";
 
-    var x = document.getElementById("artOnly");
+    var x = document.getElementById("seller_artOnly");
     x.style.display = "none";
 
-    var x = document.getElementById("weddingOnly");
+    var x = document.getElementById("seller_weddingOnly");
     x.style.display = "none";
 };
 
-function filterCraft(){
+function seller_filterCraft(){
     document.getElementById("catInfo").innerHTML = "Showing Products With Crafting Supplies Category";
 
-    var x = document.getElementById("all");
+    var x = document.getElementById("seller_all");
     x.style.display = "none";
 
-    var x = document.getElementById("clothOnly");
+    var x = document.getElementById("seller_clothOnly");
     x.style.display = "none";
 
-    var x = document.getElementById("jewOnly");
+    var x = document.getElementById("seller_jewOnly");
     x.style.display = "none";
 
-    var x = document.getElementById("craftOnly");
+    var x = document.getElementById("seller_craftOnly");
     x.style.display = "block";
 
-    var x = document.getElementById("bedOnly");
+    var x = document.getElementById("seller_bedOnly");
     x.style.display = "none";
 
-    var x = document.getElementById("toyOnly");
+    var x = document.getElementById("seller_toyOnly");
     x.style.display = "none";
 
-    var x = document.getElementById("artOnly");
+    var x = document.getElementById("seller_artOnly");
     x.style.display = "none";
 
-    var x = document.getElementById("weddingOnly");
+    var x = document.getElementById("seller_weddingOnly");
     x.style.display = "none";
 };
 
-function filterBed(){
+function seller_filterBed(){
     document.getElementById("catInfo").innerHTML = "Showing Products With Bedding/Room Décor Category";
 
-    var x = document.getElementById("all");
+    var x = document.getElementById("seller_all");
     x.style.display = "none";
 
-    var x = document.getElementById("clothOnly");
+    var x = document.getElementById("seller_clothOnly");
     x.style.display = "none";
 
-    var x = document.getElementById("jewOnly");
+    var x = document.getElementById("seller_jewOnly");
     x.style.display = "none";
 
-    var x = document.getElementById("craftOnly");
+    var x = document.getElementById("seller_craftOnly");
     x.style.display = "none";
 
-    var x = document.getElementById("bedOnly");
+    var x = document.getElementById("seller_bedOnly");
     x.style.display = "block";
 
-    var x = document.getElementById("toyOnly");
+    var x = document.getElementById("seller_toyOnly");
     x.style.display = "none";
 
-    var x = document.getElementById("artOnly");
+    var x = document.getElementById("seller_artOnly");
     x.style.display = "none";
 
-    var x = document.getElementById("weddingOnly");
+    var x = document.getElementById("seller_weddingOnly");
     x.style.display = "none";
 };
 
-function filterToy(){
+function seller_filterToy(){
     document.getElementById("catInfo").innerHTML = "Showing Products With Soft Toys Category";
 
-    var x = document.getElementById("all");
+    var x = document.getElementById("seller_all");
     x.style.display = "none";
 
-    var x = document.getElementById("clothOnly");
+    var x = document.getElementById("seller_clothOnly");
     x.style.display = "none";
 
-    var x = document.getElementById("jewOnly");
+    var x = document.getElementById("seller_jewOnly");
     x.style.display = "none";
 
-    var x = document.getElementById("craftOnly");
+    var x = document.getElementById("seller_craftOnly");
     x.style.display = "none";
 
-    var x = document.getElementById("bedOnly");
+    var x = document.getElementById("seller_bedOnly");
     x.style.display = "none";
 
-    var x = document.getElementById("toyOnly");
+    var x = document.getElementById("seller_toyOnly");
     x.style.display = "block";
 
-    var x = document.getElementById("artOnly");
+    var x = document.getElementById("seller_artOnly");
     x.style.display = "none";
 
-    var x = document.getElementById("weddingOnly");
+    var x = document.getElementById("seller_weddingOnly");
     x.style.display = "none";
 };
 
-function filterArt(){
+function seller_filterArt(){
     document.getElementById("catInfo").innerHTML = "Showing Products With Vintage Arts Category";
 
-    var x = document.getElementById("all");
+    var x = document.getElementById("seller_all");
     x.style.display = "none";
 
-    var x = document.getElementById("clothOnly");
+    var x = document.getElementById("seller_clothOnly");
     x.style.display = "none";
 
-    var x = document.getElementById("jewOnly");
+    var x = document.getElementById("seller_jewOnly");
     x.style.display = "none";
 
-    var x = document.getElementById("craftOnly");
+    var x = document.getElementById("seller_craftOnly");
     x.style.display = "none";
 
-    var x = document.getElementById("bedOnly");
+    var x = document.getElementById("seller_bedOnly");
     x.style.display = "none";
 
-    var x = document.getElementById("toyOnly");
+    var x = document.getElementById("seller_toyOnly");
     x.style.display = "none";
 
-    var x = document.getElementById("artOnly");
+    var x = document.getElementById("seller_artOnly");
     x.style.display = "block";
 
-    var x = document.getElementById("weddingOnly");
+    var x = document.getElementById("seller_weddingOnly");
     x.style.display = "none";
 };
 
-function filterWed(){
+function seller_filterWed(){
     document.getElementById("catInfo").innerHTML = "Showing Products With Wedding Accessories Category";
 
-    var x = document.getElementById("all");
+    var x = document.getElementById("seller_all");
     x.style.display = "none";
 
-    var x = document.getElementById("clothOnly");
+    var x = document.getElementById("seller_clothOnly");
     x.style.display = "none";
 
-    var x = document.getElementById("jewOnly");
+    var x = document.getElementById("seller_jewOnly");
     x.style.display = "none";
 
-    var x = document.getElementById("craftOnly");
+    var x = document.getElementById("seller_craftOnly");
     x.style.display = "none";
 
-    var x = document.getElementById("bedOnly");
+    var x = document.getElementById("seller_bedOnly");
     x.style.display = "none";
 
-    var x = document.getElementById("toyOnly");
+    var x = document.getElementById("seller_toyOnly");
     x.style.display = "none";
 
-    var x = document.getElementById("artOnly");
+    var x = document.getElementById("seller_artOnly");
     x.style.display = "none";
 
-    var x = document.getElementById("weddingOnly");
+    var x = document.getElementById("seller_weddingOnly");
     x.style.display = "block";
 };
+
